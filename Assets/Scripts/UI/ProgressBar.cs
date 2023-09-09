703f3d57cf2c0266d27ef513d18acf5195b8693c
Sangjun Lee <112681633+lsjmc0224@users.noreply.github.com>
2023-09-09 20:35:44 +0900

    ProgressBar.cs 파일 UI 폴더로 옮김

---
+++ Assets/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+    public int maximum = 25;
+    public int minimum = 0;
+    public int current { get { return GameManager.GetEnemyCounter(); } }
+    public Image mask;
+    public Image fill;
+    public Color color;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float currentOffset = current - minimum;
+        float maximumOffset = maximum - minimum;
+        float fillAmount = currentOffset / maximumOffset;
+        mask.fillAmount = fillAmount;
+
+        fill.color = color;
+    }
+}
